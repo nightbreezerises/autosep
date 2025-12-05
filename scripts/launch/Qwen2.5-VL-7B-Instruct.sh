db1c591d5709506e67045aaee942805c847aaa03
nightbreezerises <3126404715@qq.com>
2025-12-06 00:06:28 +0800

    备份

---
+++ scripts/launch/Qwen2.5-VL-7B-Instruct.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+set -x
+
+export HF_HOME="./data/huggingface"
+export TRANSFORMERS_CACHE="./data/huggingface/transformers"
+export HF_DATASETS_CACHE="./data/huggingface/datasets"
+
+MODEL_REPO=Qwen/Qwen2.5-VL-7B-Instruct
+
+GPU=$1
+PORT=30000
+
+IFS=',' read -ra GPU_ARRAY <<< "$GPU"
+
+tensor_parallel_size=${#GPU_ARRAY[@]}
+echo $tensor_parallel_size
+
+mem_fraction_static=0.8
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m sglang.launch_server \
+  --model-path $MODEL_REPO \
+  --port $PORT \
+  --tp-size $tensor_parallel_size \
+  --trust-remote-code \
+  --mem-fraction-static $mem_fraction_static \
+  --chat-template qwen2-vl \
+  --disable-radix-cache \
+  --chunked-prefill-size -1
+
+# bash scripts/launch/Qwen2-VL-7B-Instruct.sh 4,5,6,7
+# nohup bash scripts/launch/Qwen2.5-VL-7B-Instruct.sh 0 > qwen_server.log 2>&1 &
